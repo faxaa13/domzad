2846c8d388cd4a8553db1c0d218ff532551a591e
Bayramov Farkhad <115133084+faxaa13@users.noreply.github.com>
2022-12-19 23:56:46 -0800

    Update index.js

--- domzad/index.js
+++ domzad/index.js
@@ -3,4 +3,4 @@ let car = [
     ['Hyundai' , 'KIA' , 'Pantiac' , 'Chevrolet' , 'HUMMER'],
     ['Ferrari' , 'MITSUBISHI' , 'Genesis','Dodge' ]
 ]
-document.write(car[2][0][0]+car[0][2][0]+car[1][4][5]+car[1][0][0]+car[1][1][2]+car[2][3][0])
+console.log(car[2][0][0]+car[0][2][0]+car[1][4][5]+car[1][0][0]+car[1][1][2]+car[2][3][0])
