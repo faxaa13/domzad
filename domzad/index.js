d2c351e6f6fe7a1141bafd61fad373ce93be2eb1
faxaa13 <115133084+faxaa13@users.noreply.github.com>
2022-12-20 10:49:40 +0400

    first commit

---
+++ domzad/index.js
@@ -0,0 +1,6 @@
+let car = [
+    ['Mercedes' , 'BMW' , 'Audi' , 'Opel' , 'Porsche' ] ,
+    ['Hyundai' , 'KIA' , 'Pantiac' , 'Chevrolet' , 'HUMMER'],
+    ['Ferrari' , 'MITSUBISHI' , 'Genesis','Dodge' ]
+]
+document.write(car[2][0][0]+car[0][2][0]+car[1][4][5]+car[1][0][0]+car[1][1][2]+car[2][3][0])
